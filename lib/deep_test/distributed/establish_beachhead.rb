ac56de38393cfd4873758221db90ffca146f8769
Andrew/Stefania <tworker@manheim.com>
2014-03-06 09:23:14 -0600

    Join file names to find distributed tests root directory only worked by mistake

--- lib/deep_test/distributed/establish_beachhead.rb
+++ lib/deep_test/distributed/establish_beachhead.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/../../deep_test"
+require_relative "./../../deep_test"
 options = DeepTest::Options.from_command_line(ENV['OPTIONS'])
 ENV['DEEP_TEST_LOG_LEVEL'] = options.environment_log_level
 options.ssh_client_connection_info = DeepTest::Distributed::SshClientConnectionInfo.new
@@ -8,7 +8,7 @@ DeepTest.logger.debug { "mirror establish_beachhead for #{options.origin_hostnam
 STDIN.close
 
 beachhead_port = DeepTest::Distributed::Beachhead.new(
-  File.join(options.mirror_path, File.basename(options.sync_options[:source])), 
+  File.join(options.mirror_path), 
   options
 ).daemonize
 

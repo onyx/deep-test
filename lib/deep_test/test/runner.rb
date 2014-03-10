e84b59a730c7a88856a901b2972b56c8adf6182f
Andrew/Angel <tworker@manheim.com>
2014-03-10 13:07:47 -0500

    Changing Test Unit runner to handle Test Unit 2.0+

--- lib/deep_test/test/runner.rb
+++ lib/deep_test/test/runner.rb
@@ -13,10 +13,10 @@ module DeepTest
       end
 
       def process_work_units(central_command)
-        suite = ::Test::Unit::AutoRunner::COLLECTORS[:objectspace].call NO_FILTERS
+        suite = ::Test::Unit::AutoRunner::COLLECTORS["object_space"].call NO_FILTERS
         supervised_suite = DeepTest::Test::SupervisedTestSuite.new(suite, central_command)
         require 'test/unit/ui/console/testrunner'
-        result = ::Test::Unit::UI::Console::TestRunner.run(supervised_suite, ::Test::Unit::UI::NORMAL)
+        result = ::Test::Unit::UI::Console::TestRunner.run(supervised_suite)
         result.passed?
       end
     end
